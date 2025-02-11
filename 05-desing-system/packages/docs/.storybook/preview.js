ab41122b624f55e2bbdeacc3951df5c0d83840b6
Kevin Sousa <115954924+Kevin199527@users.noreply.github.com>
2025-02-11 12:12:50 -0100

    update

---
+++ 05-desing-system/packages/docs/.storybook/preview.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+};
+exports.default = preview;
+//# sourceMappingURL=preview.js.map
